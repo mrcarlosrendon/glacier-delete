15ca7cdbc397b23c21fdc231d3beeef3633bff9b
Carlos Rendon <1294785+mrcarlosrendon@users.noreply.github.com>
2020-07-26 15:34:13 -0700

    Remove command argument for unused account
    Also remove hard-coded vaultName

--- delete_archives.py
+++ delete_archives.py
@@ -2,7 +2,7 @@
 Delete's AWS Glacier archives given a Vault's inventory JSON
 Handles step 5 of https://docs.aws.amazon.com/amazonglacier/latest/dev/deleting-an-archive-using-cli.html
 
-Usage: delete_archives.py [vault_name] [account_id] [archive_json_file]
+Usage: delete_archives.py [vault_name] [archive_json_file]
 """
 
 import os
@@ -14,24 +14,22 @@ from joblib import Parallel, delayed
 
 argparser = argparse.ArgumentParser()
 argparser.add_argument("vault", help="Vault Name")
-argparser.add_argument("account", help="AWS Account ID")
 argparser.add_argument("archive_json_file", help="JSON of Archive Inventory. See step 4 https://docs.aws.amazon.com/amazonglacier/latest/dev/deleting-an-archive-using-cli.html")
 args = argparser.parse_args()
 
 VAULT_NAME = args.vault
-ACCOUNT_ID = args.account
 
 with open(args.archive_json_file) as f:
     j = json.load(f)
 
 archives = j.get('ArchiveList')
-
 print("Deleting " + str(len(archives)) + " archives.")
+
 
 def delete_boto(archive):
     client = boto3.client("glacier")
     archive_id = archive.get("ArchiveId")    
-    client.delete_archive(vaultName='backup', archiveId=archive_id)
+    client.delete_archive(vaultName=args.vault, archiveId=archive_id)
 
 
 Parallel(n_jobs=os.cpu_count(), prefer="threads", verbose=1)(delayed(delete_boto)(archive) for archive in archives)
