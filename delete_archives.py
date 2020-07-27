1ecd2ab918374fdb98f1f453e6606a551d1e936e
Carlos Rendon <1294785+mrcarlosrendon@users.noreply.github.com>
2020-07-26 21:15:08 -0700

    Add exception handling. Formatting for pep8

--- delete_archives.py
+++ delete_archives.py
@@ -1,6 +1,7 @@
 """
 Delete's AWS Glacier archives given a Vault's inventory JSON
-Handles step 5 of https://docs.aws.amazon.com/amazonglacier/latest/dev/deleting-an-archive-using-cli.html
+Handles step 5 of:
+https://docs.aws.amazon.com/amazonglacier/latest/dev/deleting-an-archive-using-cli.html
 
 Usage: delete_archives.py [vault_name] [archive_json_file]
 """
@@ -14,7 +15,10 @@ from joblib import Parallel, delayed
 
 argparser = argparse.ArgumentParser()
 argparser.add_argument("vault", help="Vault Name")
-argparser.add_argument("archive_json_file", help="JSON of Archive Inventory. See step 4 https://docs.aws.amazon.com/amazonglacier/latest/dev/deleting-an-archive-using-cli.html")
+help = "JSON of Archive Inventory. See step 4 of " \
+       "https://docs.aws.amazon.com/amazonglacier/latest/dev/deleting-an-archive-using-cli.html"
+argparser.add_argument("archive_json_file",
+                       help=help)
 args = argparser.parse_args()
 
 with open(args.archive_json_file) as f:
@@ -26,8 +30,15 @@ print("Deleting " + str(len(archives)) + " archives.")
 
 def delete_boto(archive):
     client = boto3.client("glacier")
-    archive_id = archive.get("ArchiveId")    
-    client.delete_archive(vaultName=args.vault, archiveId=archive_id)
+    archive_id = archive.get("ArchiveId")
+    try:
+        resp = client.delete_archive(vaultName=args.vault,
+                                     archiveId=archive_id)
+    except Exception as e:
+        print(resp)
+        print(e)
 
 
-Parallel(n_jobs=os.cpu_count(), prefer="threads", verbose=1)(delayed(delete_boto)(archive) for archive in archives)
+Parallel(n_jobs=os.cpu_count(),
+         prefer="threads",
+         verbose=1)(delayed(delete_boto)(archive) for archive in archives)
